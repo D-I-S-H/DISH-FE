66c2ad60040d0da25afd4d1d99112d4710edc6a0
That-Thing <sen@onee.ch>
2024-10-16 13:31:26 -0400

    Changed token to uid and added register path.

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -4,8 +4,8 @@
         <div class="col-12 col-md-6 col-lg-4">
           <div class="card p-4">
             <div class="card-body">
-              <h5 class="card-title">Sign In</h5>
-              <form @submit.prevent="signIn">
+              <h5 class="card-title">Register</h5>
+              <form @submit.prevent="register">
                 <div class="form-group mb-3">
                   <label for="username">Username</label>
                   <input type="text" class="form-control" id="username" v-model="username" required>
@@ -13,6 +13,10 @@
                 <div class="form-group mb-3">
                   <label for="password">Password</label>
                   <input type="password" class="form-control" id="password" v-model="password" required>
+                </div>
+                <div class="form-group mb-3">
+                  <label for="confirm-password">Confirm Password</label>
+                  <input type="password" class="form-control" id="confirm-password" v-model="confirmPassword" required>
                 </div>
                 <button type="submit" class="btn btn-primary w-100 mt-3">Sign In</button>
               </form>
@@ -53,9 +57,13 @@ export default {
     /**
      * Sign in the user using the provided username and password
      */
-    async signIn() {
+    async register() {
       try {
-        const response = await axios.post(`${API_URL}/login`, {
+        if(this.password !== this.confirmPassword){
+          $toast.error('Passwords do not match');
+          return;
+        }
+        const response = await axios.post(`${API_URL}/register`, {
           username: this.username,
           password: this.password
         });
@@ -64,11 +72,11 @@ export default {
           localStorage.setItem('user', JSON.stringify(response.data.user));
           this.router.push('/'); // Use this.router to navigate
         } else {
-          $toast.error('Login failed');
+          $toast.error('Register failed');
         }
       } catch (error) {
-        console.error('Login failed', error);
-        $toast.error('Login failed');
+        console.error('Register failed', error);
+        $toast.error('Register failed');
       }
     }
   }
